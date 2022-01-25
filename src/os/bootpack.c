fd32bf16bc01168f11011b7eb4f064dc12977ca3
foura <togo33937@gmail.com>
2022-01-25 17:51:56 +0800

    1.鼠标数据通信

--- src/os/bootpack.c
+++ src/os/bootpack.c
@@ -1,19 +1,29 @@
 #include "bootpack.h"
 #include <stdio.h>
 
-extern struct FIFO8 keyfifo;
+struct MOUSE_DEC {
+	unsigned char buf[3], phase;
+	int x, y, btn;
+};
+
+extern struct FIFO8 keyfifo,  mousefifo;
+void enable_mouse(struct MOUSE_DEC *mdec);
+void init_keyboard(void);
+int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat);
 
 void HariMain(void)
 {
 	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
-	char s[40], mcursor[256], keybuf[32];
+	char s[40], mcursor[256], keybuf[32], mousebuf[128];
 	int mx, my, i, j;
+	struct MOUSE_DEC mdec;
 
 	init_gdtidt();
 	init_pic();
 	io_sti(); /*由于IDT/PIC的初始化完成，CPU中断禁止被取消。  */
 
 	fifo8_init(&keyfifo, 32, keybuf);
+	fifo8_init(&mousefifo, 128, mousebuf);
 	io_out8(PIC0_IMR, 0xf9); /* 允许 PIC1 和键盘 (11111001) */
 	io_out8(PIC1_IMR, 0xef); /* 允许鼠标 (11101111) */
 
@@ -28,18 +38,40 @@ void HariMain(void)
 	sprintf(s, "(%d, %d)", mx, my);
 	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 
-	enable_mouse();
+	enable_mouse(&mdec);
 
 	for (;;) {
 		io_cli();
-		if (fifo8_status(&keyfifo) == 0) {
+		if (fifo8_status(&keyfifo) + fifo8_status(&mousefifo) == 0) {
 			io_stihlt();
 		} else {
-			i = fifo8_get(&keyfifo);
-			io_sti();
-			sprintf(s, "%02X", i);
-			boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 16, 15, 31);
-			putfonts8_asc(binfo->vram, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
+			if (fifo8_status(&keyfifo) != 0) {
+				i = fifo8_get(&keyfifo);
+				io_sti();
+				sprintf(s, "%02X", i);
+				boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 16, 15, 31);
+				putfonts8_asc(binfo->vram, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
+			} else if (fifo8_status(&mousefifo) != 0) {
+				i = fifo8_get(&mousefifo);
+				io_sti();
+				if (mouse_decode(&mdec, i) != 0) {
+					/* 鼠标的3个字节都齐了显示出来 */
+					sprintf(s, "[lcr %4d %4d]", mdec.x, mdec.y);
+					if ((mdec.btn & 0x01) != 0) {
+						s[1] = 'L';
+					}
+					if ((mdec.btn & 0x02) != 0) {
+						s[3] = 'R';
+					}
+					if ((mdec.btn & 0x04) != 0) {
+						s[2] = 'C';
+					}
+					boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 32 + 15 * 8 - 1, 31);
+					putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+				}
+					
+			}
+
 		}
 	}
 }
@@ -74,12 +106,58 @@ void init_keyboard(void)
 #define KEYCMD_SENDTO_MOUSE		0xd4
 #define MOUSECMD_ENABLE			0xf4
 
-void enable_mouse(void)
+void enable_mouse(struct MOUSE_DEC *mdec)
 {
 	/* 激活鼠标 */
 	wait_KBC_sendready();
 	io_out8(PORT_KEYCMD, KEYCMD_SENDTO_MOUSE);
 	wait_KBC_sendready();
 	io_out8(PORT_KEYDAT, MOUSECMD_ENABLE);
-	return; /* 顺利的话,键盘控制其会返送回 ACK(0xfa)*/
+ 	/* 顺利的话,键盘控制其会返送回 ACK(0xfa)*/
+	mdec->phase = 0; // 等待0xfa的阶段
+	return; 
 }
+
+int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat)
+{
+	if (mdec->phase == 0) {
+		/* 等待鼠标的0xfa的状态 */
+		if (dat == 0xfa) {
+			mdec->phase = 1;
+		}
+		return 0;
+	} 
+
+	if (mdec->phase == 1) {
+		/* 等待鼠标的第一字节 */
+		if ((dat & 0xc8) == 0x08) {
+			mdec->buf[0] = dat;
+			mdec->phase = 2;
+		}
+		return 0;
+	} 
+	if (mdec->phase == 2) {
+		/* 等待鼠标的第二字节 */
+		mdec->buf[1] = dat;
+		mdec->phase = 3;
+		return 0;
+	} 
+	if (mdec->phase == 3) {
+		/* 等待鼠标的第三字节 */
+		mdec->buf[2] = dat;
+		mdec->phase = 1;
+		mdec->btn = mdec->buf[0] & 0x07;
+		mdec->x = mdec->buf[1];
+		mdec->y = mdec->buf[2];
+		if ((mdec->buf[0] & 0x10) != 0) {
+			mdec->x |= 0xffffff00;
+		}
+		if ((mdec->buf[0] & 0x20) != 0) {
+			mdec->y |= 0xffffff00;
+		}
+		mdec->y = - mdec->y;
+		return 1;
+	}
+
+	return -1;
+}  
--- src/os/int.c
+++ src/os/int.c
@@ -33,15 +33,17 @@ void inthandler21(int *esp)
 	fifo8_put(&keyfifo, data);
 }
 
+struct FIFO8 mousefifo;
+
 // 鼠标中断
 void inthandler2c(int *esp) 
 {
-	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
-	boxfill8(binfo->vram, binfo->scrnx, COL8_000000, 0, 0, 32 * 8 - 1, 15);
-	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, "INT 2C (IRQ-12) : PS/2 mouse");
-	for (;;) {
-		io_hlt();
-	}
+	unsigned char data;
+	io_out8(PIC1_OCW2, 0x64);	/* 通知从PIC1 IRQ-12的受理已经完成*/
+	io_out8(PIC0_OCW2, 0x62);	/* 通知主PIC0 IRQ-02的受理已经完成 */
+	data = io_in8(PORT_KEYDAT);
+	fifo8_put(&mousefifo, data);
+	return;
 }
 
 /*针对来自PIC0的不完全中断的对策*/
